671f2b625b70de171dc29260c1e14a985d4d1446
Peter Deyi <peterdeyi@icloud.com>
2023-05-11 13:39:40 -0700

    Initial Commit

---
+++ asyncImage_Panda/asyncImage_PandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncImage_PandaApp.swift
+//  asyncImage_Panda
+//
+//  Created by Peter Deyi on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct asyncImage_PandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
